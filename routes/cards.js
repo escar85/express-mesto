7ba5b3514157d5adc4bf068a93c220c72a9a6347
Melnik Dmitry <escar1985@yandex.ru>
2020-10-01 12:54:26 +0300

    исправить проект

--- routes/cards.js
+++ routes/cards.js
@@ -5,12 +5,12 @@ const fs = require('fs').promises;
 cardsRouter.get('/cards', (req, res) => {
   const filePath = path.resolve('data', 'cards.json');
   fs.readFile(filePath, { encoding: 'utf8' })
-  .then(data => JSON.parse(data))
-  .then(data => res.send(data))
-  .catch(err => {
-    console.log(err);
-    res.status(500).send({ message: 'Ошибка на сервере'})
-  })
+    .then(data => JSON.parse(data))
+    .then(data => res.send(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ message: 'Ошибка на сервере' })
+    })
 });
 
-  module.exports = cardsRouter;
+module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -6,31 +6,31 @@ const filePath = path.resolve('data', 'users.json');
 
 usersRouter.get('/users', (req, res) => {
   fs.readFile(filePath, { encoding: 'utf8' })
-  .then(data => JSON.parse(data))
-  .then(data => res.send(data))
-  .catch(err => {
-    console.log(err);
-    res.status(500).send({ message: 'Ошибка на сервере'})
-  })
+    .then(data => JSON.parse(data))
+    .then(data => res.send(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ message: 'Ошибка на сервере' })
+    })
 })
 
 const sendUserById = (req, res) => {
   fs.readFile(filePath, { encoding: 'utf8' })
-  .then(data => JSON.parse(data))
-  .then(users => {
-    const isUserId = users.some((user) => user._id == req.params.id);
+    .then(data => JSON.parse(data))
+    .then(users => {
+      const user = users.find(user => user._id == req.params.id);
 
-    if (!isUserId) {
-      res.status(404).send({message: '«Нет пользователя с таким id»'});
-      return;
-    }
+      if (!user) {
+        res.status(404).send({ message: '«Нет пользователя с таким id»' });
+        return;
+      }
 
-    res.send(users.find(user => user._id == req.params.id))
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).send({ message: 'Ошибка на сервере'})
-  })
+      res.send(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ message: 'Ошибка на сервере' })
+    })
 };
 
 usersRouter.get('/users/:id', sendUserById);
