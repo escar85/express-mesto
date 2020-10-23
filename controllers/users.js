2620d624158605573860dc27d1f68070310cb845
Melnik Dmitry <escar1985@yandex.ru>
2020-10-23 11:47:31 +0300

    проект готов

--- controllers/users.js
+++ controllers/users.js
@@ -26,9 +26,9 @@ const createUser = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => User.create({
       name: req.body.name,
-      about: req.body.about,
-      avatar: req.body.avatar,
-      email: req.body.email,
+      // about: req.body.about,
+      // avatar: req.body.avatar,
+      // email: req.body.email,
       password: hash
     }))
     .then(user => res.send({ data: user }))
