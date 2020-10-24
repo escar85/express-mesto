0e909552594d39f08bb20024adb594d082acbe4b
Melnik Dmitry <escar1985@yandex.ru>
2020-10-24 17:26:30 +0300

    проект готов

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'https://escar.students.nomoreparties.space/sign-in');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin, Access-Control-Allow-Origin, X-Requested-With, Content-Type ,Accept ,content-type, application/json');
