c572a854cc32da5be7a6fca767626d6b002983bf
Melnik Dmitry <escar1985@yandex.ru>
2020-10-25 13:53:17 +0300

    проект готов

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin, Access-Control-Allow-Origin, Authorization, X-Requested-With, Content-Type, Accept, content-type, application/json');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Origin, Access-Control-Allow-Origin, Authorization, X-Requested-With, Content-Type, Accept, content-type, application/json');
+//   next();
+// });
 
 app.use(express.json());
 
